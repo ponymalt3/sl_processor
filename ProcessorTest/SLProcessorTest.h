e1d07d91488386bdbd3fd7a79c1b54131257fd40
me <laama@t-online.de>
2016-06-05 23:11:32 +0200

    extend rtasm test header; adapt processor test class

--- ProcessorTest/SLProcessorTest.h
+++ ProcessorTest/SLProcessorTest.h
@@ -11,10 +11,22 @@
 #include <assert.h>
 #include "../SLProcessor.h"
 #include "../SLCodeDef.h"
+#include "qfp32.h"
 
 class LoadAndSimulateProcessor
 {
 public:
+  LoadAndSimulateProcessor()
+    : localMem_(512)
+    , codeMem_(256)
+    , extMem_(16*1024)
+    , codePort_(codeMem_.createPort())
+    , localPort_(localMem_.createPort())
+    , extPort_(extMem_.createPort()) 
+    , processor_(localMem_,extMem_.createPort(),codeMem_.createPort())
+  {
+  }
+  
   template<const uint32_t CodeSize>
   LoadAndSimulateProcessor(uint32_t (&code)[CodeSize])
     : localMem_(512)
@@ -32,7 +44,13 @@ public:
     }
   }
   
-  void writeMemory(qfp32 addr,uint32_t value)
+  void writeCode(uint32_t addr,uint16_t value)
+  {
+    codePort_.write(addr,value);
+    codePort_.update();
+  }
+  
+  void writeMemory(qfp32_t addr,uint32_t value)
   {
     writeMemory((int32_t)addr,value);
   }
@@ -51,7 +69,7 @@ public:
     }
   }
   
-  uint32_t readMemory(qfp32 addr)
+  uint32_t readMemory(qfp32_t addr)
   {
     return readMemory((int32_t)addr);
   }
@@ -95,6 +113,14 @@ public:
   {
     for(uint32_t i=0;i<cycles;++i)
       processor_.update(0,1,pcValue);
+  }
+  
+  void executeUntilAddr(uint32_t addr)
+  {
+    while(processor_.getExecutedAddr() != addr)
+    {
+      processor_.update(0,0,0);
+    }
   }
     
 protected:
--- RTAsmTest/RTAsmTest.h
+++ RTAsmTest/RTAsmTest.h
@@ -1,20 +1,75 @@
 #pragma once
 
+#include <stdint.h>
 #include "RTAsm/RTParser.h"
 #include "RTAsm/Error.h"
 #include "RTAsm/RTProg.h"
+#include "../ProcessorTest/SLProcessorTest.h"
 
 class RTProgTester
 {
 public:
   RTProgTester(RTProg prog):prog_(prog),s_(prog_),codeGen_(s_),parser_(codeGen_)
   {
+    //zero memory (at least first 256 words)
+    for(uint32_t i=0;i<256;++i)
+    {
+      proc_.writeMemory(i,0);
+    }  
   }
   
-  Error parse()
+  Error parse(uint32_t reserveParamter=0)
   {
     parser_.parse(s_);
+    codeGen_.storageAllocationPass<512>(reserveParamter);
     return prog_.getErrorHandler();
+  }
+  
+  uint16_t getCodeAt(uint32_t addr)
+  {
+    return codeGen_.getCodeAt(addr);
+  }
+  
+  uint32_t getIRSAddrOfSymbol(const char *symbol)
+  {
+    uint32_t len=0;
+    while(symbol[len] != '\0') ++len;
+    
+    SymbolMap::_Symbol s=codeGen_.findSymbol(Stream::String(symbol,0,len));
+    
+    if(s.strLength_ != 0)
+    {
+      return s.allocatedAddr_;
+    }
+    
+    return 0xFFFFFFFF;
+  }
+  
+  LoadAndSimulateProcessor& getProcessor()
+  {
+    return proc_;
+  }
+  
+  void loadCode()
+  {
+    std::cout<<"code:\n";
+    for(uint32_t i=0;i<codeGen_.getCurCodeAddr();++i)
+    {
+      std::cout<<std::hex<<"  "<<(getCodeAt(i))<<"\n";
+      proc_.writeCode(i,getCodeAt(i));
+    }
+    std::cout<<std::dec<<"\n";
+    
+    for(uint32_t i=codeGen_.getCurCodeAddr();i<codeGen_.getCurCodeAddr()+3;++i)
+    {
+      proc_.writeCode(i,0xFFFF);
+    }  
+  }
+  
+  void execute()
+  {    
+    proc_.reset();
+    proc_.executeUntilAddr(codeGen_.getCurCodeAddr()-1);
   }
   
   
@@ -22,5 +77,6 @@ protected:
   RTProg prog_;
   Stream s_;
   CodeGen codeGen_;
-  RTParser parser_;  
+  RTParser parser_;
+  LoadAndSimulateProcessor proc_;
 };
