4036526660d7ebfc17b1eacec03c951e10a27de2
me <laama@t-online.de>
2016-06-09 22:17:10 +0200

    add tests for testAssignADx with addr inc

--- RTAsmTest/testAssignADx.cpp
+++ RTAsmTest/testAssignADx.cpp
@@ -97,3 +97,81 @@ MTEST(testAssignADx,test_that_array_assigned_to_addr0_and_deref_addr0_works)
   
   EXPECT(tester.getProcessor().readMemory(2) == qfp32_t(3).asUint);  
 }
+
+MTEST(testAssignADx,test_that_def_const_assigned_to_addr1_and_deref_addr1_works)
+{
+  RTProg testAssign=RTASM(
+    def x 10;
+    a1=x;
+    [a1]=x;
+  );
+  
+  RTProgTester tester(testAssign);
+  EXPECT(tester.parse().getNumErrors() == 0);
+  
+  tester.loadCode();
+  tester.execute();
+   
+  EXPECT(tester.getProcessor().readMemory(10) == qfp32_t(10).asUint);  
+}
+
+MTEST(testAssignADx,test_that_var_assigned_to_deref_addr0_with_inc_works)
+{
+  RTProg testAssign=RTASM(
+    a=7;
+    a0=5;
+    [a0++]=a;
+    [a0]=99;
+  );
+  
+  RTProgTester tester(testAssign);
+  EXPECT(tester.parse().getNumErrors() == 0);
+  
+  tester.loadCode();
+  tester.execute();
+  
+  EXPECT(tester.getProcessor().readMemory(5) == qfp32_t(7).asUint); 
+  EXPECT(tester.getProcessor().readMemory(6) == qfp32_t(99).asUint); 
+}
+
+MTEST(testAssignADx,test_that_var_assigned_to_deref_addr1_with_inc_works)
+{
+  RTProg testAssign=RTASM(
+    a=7;
+    a1=5;
+    [a1++]=a;
+    [a1]=99;
+  );
+  
+  RTProgTester tester(testAssign);
+  EXPECT(tester.parse().getNumErrors() == 0);
+  
+  tester.loadCode();
+  tester.execute();
+  
+  EXPECT(tester.getProcessor().readMemory(5) == qfp32_t(7).asUint); 
+  EXPECT(tester.getProcessor().readMemory(6) == qfp32_t(99).asUint); 
+}
+
+MTEST(testAssignADx,test_that_deref_addr0_assigned_to_deref_addr1_with_both_inc_works)
+{
+  RTProg testAssign=RTASM(
+    a0=7;
+    a1=5;
+    [a0]=700;
+    [a1++]=[a0++];
+    [a0]=27;
+    [a1]=43;
+  );
+    
+  RTProgTester tester(testAssign);
+  EXPECT(tester.parse().getNumErrors() == 0);
+  
+  tester.loadCode();
+  tester.execute();
+  
+  EXPECT(tester.getProcessor().readMemory(7) == qfp32_t(700).asUint);
+  EXPECT(tester.getProcessor().readMemory(5) == qfp32_t(700).asUint);
+  EXPECT(tester.getProcessor().readMemory(8) == qfp32_t(27).asUint);
+  EXPECT(tester.getProcessor().readMemory(6) == qfp32_t(43).asUint); 
+}
