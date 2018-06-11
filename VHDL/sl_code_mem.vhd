7b56e31d7856ec926b5db22251063a9358952549
malte <laama@t-online.de>
2018-06-11 13:55:33 +0200

    adapt code_mem ifc and behaviour

--- VHDL/sl_code_mem.vhd
+++ VHDL/sl_code_mem.vhd
@@ -14,18 +14,22 @@ entity sl_code_mem is
     clk_i      : in  std_ulogic;
     reset_n_i  : in  std_ulogic;
 
-    p0_addr_i  : in  unsigned(15 downto 0);
-    p0_code_o  : out std_ulogic_vector(15 downto 0);
-    p0_core_en_i : in std_ulogic;
-    p1_addr_i  : in  unsigned(15 downto 0);
-    p1_code_o  : out std_ulogic_vector(15 downto 0);
-    p1_core_en_i : in std_ulogic;
-    p2_addr_i  : in  unsigned(15 downto 0);
-    p2_code_o  : out std_ulogic_vector(15 downto 0);
-    p2_core_en_i : in std_ulogic;
-    p3_addr_i  : in  unsigned(15 downto 0);
-    p3_code_o  : out std_ulogic_vector(15 downto 0);
-    p3_core_en_i : in std_ulogic;
+    p0_addr_i    : in  unsigned(15 downto 0);
+    p0_code_o    : out std_ulogic_vector(15 downto 0);
+    p0_re_i      : in  std_ulogic;
+    p0_reset_n_i : in  std_ulogic;
+    p1_addr_i    : in  unsigned(15 downto 0);
+    p1_code_o    : out std_ulogic_vector(15 downto 0);
+    p1_re_i      : in  std_ulogic;
+    p1_reset_n_i : in  std_ulogic;
+    p2_addr_i    : in  unsigned(15 downto 0);
+    p2_code_o    : out std_ulogic_vector(15 downto 0);
+    p2_re_i      : in  std_ulogic;
+    p2_reset_n_i : in  std_ulogic;
+    p3_addr_i    : in  unsigned(15 downto 0);
+    p3_code_o    : out std_ulogic_vector(15 downto 0);
+    p3_re_i      : in  std_ulogic;
+    p3_reset_n_i : in  std_ulogic;
     
     wb_slave_i : in  wb_slave_ifc_in_t;
     wb_slave_o : out wb_slave_ifc_out_t);
@@ -47,13 +51,14 @@ architecture rtl of sl_code_mem is
 
   signal p2_addr : unsigned(15 downto 0);
   signal p3_addr : unsigned(15 downto 0);
+  signal p2_addr2 : unsigned(15 downto 0);
+  signal p3_addr2 : unsigned(15 downto 0);
 
   type mem_t is array (natural range <>) of std_ulogic_vector(15 downto 0);
   signal mem : mem_t(SizeInKBytes*512-1 downto 0);
 
-  signal reset : std_ulogic;
-  signal reset_1d : std_ulogic;
-  signal reset_2d : std_ulogic;
+  signal code_data_at_0 : std_ulogic_vector(15 downto 0);
+  signal load_code_data_0 : std_ulogic;
 
 begin  -- architecture rtl
 
@@ -78,35 +83,30 @@ begin  -- architecture rtl
       ack <= '0';
       p0_code_o <= (others => '0');
       p1_code_o <= (others => '0');
-      p2_addr <= to_unsigned(0,16);
-      p3_addr <= to_unsigned(0,16);
-      reset <= '1';
-      reset_1d <= '0';
-      reset_2d <= '0';
+      load_code_data_0 <= '0';
     elsif clk_i'event and clk_i = '1' then  -- rising clock edge
-      reset <= '0';
-      reset_1d <= reset;
-      reset_2d <= reset_1d;
       
-        if (state = '0' and p0_core_en_i = '1') or reset_2d = '1' then
+      if state = '0' then
+        if p0_re_i = '1' then
           p0_code_o <= rdata0;
         end if;
-        if (state = '0' and p1_core_en_i = '1') or reset_2d = '1' then
+        if p1_re_i = '1' then
           p1_code_o <= rdata1;
-        end if;
-
-      if state = '1' then
-        if p2_core_en_i = '1' then
-          p2_addr <= p2_addr_i;
-        end if;
-        if p3_core_en_i = '1' then
-          p3_addr <= p3_addr_i;
         end if;
       end if;
 
+      if p0_reset_n_i = '0' or load_code_data_0 = '1' then
+        p0_code_o <= code_data_at_0;
+      end if;
+      
+      if p1_reset_n_i = '0' or load_code_data_0 = '1' then
+        p1_code_o <= code_data_at_0;
+      end if;
+      
+      load_code_data_0 <= '0';
       if mem0_we = '1' and mem0_addr = to_unsigned(0,16) then
-        p0_code_o <= wb_slave_i.dat(15 downto 0);
-        p1_code_o <= wb_slave_i.dat(15 downto 0);
+        code_data_at_0 <= wb_slave_i.dat(15 downto 0);
+        load_code_data_0 <= '1';
       end if;
 
       state <= not state;
@@ -119,35 +119,20 @@ begin  -- architecture rtl
     end if;
   end process;
 
-  process (clk_i, reset_n_i) is
-  begin  -- process
-    if reset_n_i = '0' then             -- asynchronous reset (active low)
-      
-    elsif clk_i'event and clk_i = '0' then  -- falling clock edge
-      
-    end if;
-  end process;
-
-  process (ack, p0_addr_i, p1_addr_i, p2_addr, p3_addr, state,
-           wb_slave_i, reset_n_i) is
+  process (ack, p0_addr_i, p1_addr_i, p2_addr_i, p3_addr_i, state, wb_slave_i) is
   begin  -- process
     mem0_we <= '0';
     mem0_addr <= p0_addr_i;
     mem1_addr <= p1_addr_i;
 
     if state = '0' then
-      mem0_addr <= p2_addr;
-      mem1_addr <= p3_addr;
+      mem0_addr <= p2_addr_i;
+      mem1_addr <= p3_addr_i;
     end if;
 
     if wb_slave_i.cyc = '1' and wb_slave_i.stb = '1' and wb_slave_i.we = '1' then
       mem0_addr <= wb_slave_i.adr(15 downto 0);
       mem0_we <= '1';
-    end if;
-
-    if reset_n_i = '0' then
-      mem0_addr <= to_unsigned(0,16);
-      mem1_addr <= to_unsigned(0,16);
     end if;
 
     wb_slave_o.ack <= ack;
