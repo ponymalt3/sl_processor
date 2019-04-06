0d0dd7cff840f29f7c033d8b1eae469e878f3998
fucku <aurux@web.de>
2019-04-06 08:42:43 +0200

    RTAsm: extend RTProg to load file and append code

--- RTAsm/RTProg.h
+++ RTAsm/RTProg.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+#include <fstream>
+#include <sstream>
+
 #include "ErrorHandler.h"
 
 class RTProg
@@ -10,11 +14,33 @@ public:
     code_=code;
   }
   
-  const char* getCode() { return code_; }
+  RTProg(const std::string &code)
+  {
+    code_=code;
+  }
+  
+  void append(const char *code)
+  {
+    code_+=code;
+  }
+  
+  const char* getCode() { return code_.c_str(); }
   ErrorHandler& getErrorHandler() { return errors_; }
   
+  static RTProg createFromFile(const std::string &filename)
+  {
+    std::fstream f(filename,std::ios::in|std::ios::binary);
+    
+    if(f.is_open())
+    {
+      std::stringstream ss;
+      ss << f.rdbuf();
+      return RTProg(ss.str());
+    }
+  }
+  
 protected:
-  const char *code_;
+  std::string code_;
   ErrorHandler errors_;
 };
 
