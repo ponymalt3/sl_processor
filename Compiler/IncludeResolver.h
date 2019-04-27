03da0657da79f10c42125af06b14be5b268b56dd
fucku <aurux@web.de>
2019-04-27 09:57:36 +0200

    make include resolver track file to file mapping

--- Compiler/IncludeResolver.h
+++ Compiler/IncludeResolver.h
@@ -1,4 +1,4 @@
-#pragma concept
+#pragma once
 
 #include <iostream>
 #include <fstream>
@@ -6,13 +6,16 @@
 #include <sstream>
 #include <regex>
 #include <set>
+#include <map>
+#include <vector>
 
 class IncludeResolver
 {
 public:
   IncludeResolver(const std::string &filename)
   {
-    resolvedCode_=resolveIncludes(filename);    
+    curLine_=0;
+    resolveIncludes(filename);    
   }
   
   void storeResolvedCode(const std::string &filename)
@@ -30,16 +33,29 @@ public:
     return resolvedCode_;
   }
   
-protected:
-  std::string resolveIncludes(const std::string &filename)
+  
+  const std::map<uint32_t,std::string>& getLineToFileMap() const
   {
+    return lineToFileMap_;
+  }
+  
+  const std::map<std::string,std::vector<std::string>>& getFilesAsLineVectors() const
+  {
+    return includedFiles_;
+  }
+  
+protected:
+  void resolveIncludes(const std::string &file)
+  {
+    std::string filename=file;
+    
     std::cout<<"filename: "<<(filename)<<"\n";
     std::fstream f(filename,std::ios::in);
     
     if(!f.is_open())
     {
       std::cout<<"File '"<<(filename)<<"' not found\n";
-      return "";
+      return;
     }
     
     std::string data;
@@ -49,34 +65,62 @@ protected:
     f.seekg(0,std::ios::beg);
     
     data.resize(size);
-    f.read(const_cast<char*>(data.c_str()),size);
+    f.read(const_cast<char*>(data.c_str()),size);    
 
     std::string path="";
     std::string::size_type slashPos=filename.rfind('/');
     if(slashPos != std::string::npos)
     {
       path=filename.substr(0,slashPos);
+      filename=filename.substr(slashPos+1);
     }
+    
+    std::vector<std::string> &lines=includedFiles_.insert(std::make_pair(filename,std::vector<std::string>())).first->second;
+    lineToFileMap_.insert(std::make_pair(curLine_,filename));
     
     std::regex regex("include\\((.*)\\)");
-    std::smatch m;
-    while(std::regex_search(data,m,regex))
-    {
-      std::string toReplace=m[0].str();
-      
-      if(alreadyIncludedFiles_.find(toReplace) != alreadyIncludedFiles_.end())
-      {
-        continue;
-      }
-      
-      alreadyIncludedFiles_.insert(toReplace);
-      std::string replaceData=resolveIncludes(path + "/" + m[1].str());
-      data.replace(data.find(toReplace),toReplace.size(),replaceData);
-    }
     
-    return data;
+    uint32_t beg=0;
+    uint64_t pos=data.find('\n');
+    while(pos != std::string::npos)
+    {
+      std::string line=data.substr(beg,pos-beg+1);
+      lines.push_back(line);
+      beg=pos+1;
+      pos=data.find('\n',beg);
+      
+      std::cout<<"line: "<<(line)<<"\n";
+      
+      std::smatch m;
+      if(std::regex_search(line,m,regex))
+      {
+        std::string toReplace=m[0].str();
+        std::cout<<"toReplace: "<<(toReplace)<<"\n";
+        
+        line.replace(line.find(toReplace),toReplace.length(),"");
+        resolvedCode_+=line;
+        ++curLine_;
+        
+        //check if already included
+        if(includedFiles_.find(toReplace) != includedFiles_.end())
+        {          
+          continue;
+        }
+        
+        resolveIncludes(path + "/" + m[1].str());
+        lineToFileMap_.insert(std::make_pair(curLine_,filename));
+      }
+      else
+      {
+        resolvedCode_+=line;
+        ++curLine_;
+      }
+    }
   }
   
   std::string resolvedCode_;
-  std::set<std::string> alreadyIncludedFiles_;  
+  uint32_t curLine_;
+  std::set<std::string> alreadyIncludedFiles_;
+  std::map<std::string,std::vector<std::string> > includedFiles_;
+  std::map<uint32_t,std::string> lineToFileMap_;
 };
